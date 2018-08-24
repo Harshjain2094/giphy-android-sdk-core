f8cebb2e44ec99fa81a6e79bf7aff1e20cd7399b
Bogdan Tirca <bogdantirca92@gmail.com>
2018-08-24 11:50:07 +0300

    Bog timezone fixes

--- app/src/main/java/com/giphy/sdk/core/models/json/DateDeserializer.java
+++ app/src/main/java/com/giphy/sdk/core/models/json/DateDeserializer.java
@@ -25,11 +25,6 @@ public class DateDeserializer implements JsonDeserializer<Date> {
     private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
     private final DateFormat dateFormatStories = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
 
-    public DateDeserializer(){
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        dateFormatStories.setTimeZone(TimeZone.getTimeZone("UTC"));
-    }
-
     public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
         Date date = null;
